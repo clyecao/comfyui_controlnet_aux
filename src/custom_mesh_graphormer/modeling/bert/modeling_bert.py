b466a231f6ac0bcef5cf2230a7dd2f2effb51774
alec_c <cleiisme@gmail.com>
2025-03-04 07:41:17 +0800

    fix issue: cannot import name 'BertConfig' from 'custom_mesh_graphormer.modeling.bert.modeling_bert'

--- src/custom_mesh_graphormer/modeling/bert/modeling_bert.py
+++ src/custom_mesh_graphormer/modeling/bert/modeling_bert.py
@@ -1,1 +1,5 @@
 from transformers.models.bert.modeling_bert import *
+
+for symbol in dir(modeling_bert):
+    if not symbol.startswith("_"):
+        globals()[symbol] = getattr(modeling_bert, symbol)
